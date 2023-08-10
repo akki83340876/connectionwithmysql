83b1559d3021edbc24417109c358757ce301bf91
akki83340876 <133499635+akki83340876@users.noreply.github.com>
2023-08-10 19:10:29 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,6 @@
+const http = require('http')
+const app = require('./index')
+const port=3001;
+const server=http.createServer(app);
+server.listen(port);
+console.log("server active on port 3001");
